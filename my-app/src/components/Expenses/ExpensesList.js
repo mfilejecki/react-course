685577a612743e33e52af7ee990ba2e6304dd971
Marcel Filejecki <filej1025@gmail.com>
2022-10-24 14:37:46 +0200

    repaired small bug

--- my-app/src/components/Expenses/ExpensesList.js
+++ my-app/src/components/Expenses/ExpensesList.js
@@ -4,7 +4,7 @@ import ExpenseItem from "./ExpenseItem";
 import "./ExpensesList.css";
 
 const ExpensesList = (props) => {
-  if (props.data.length > 0) {
+  if (props.data.length === 0) {
     return <h2 className="expenses-list__fallback">Found no expenses.</h2>;
   }
 
