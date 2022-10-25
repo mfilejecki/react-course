26b2a4bf86acb3bc28e6982de8ae4bd6c9edede9
Marcel Filejecki <filej1025@gmail.com>
2022-10-25 10:29:42 +0200

    app name change

---
+++ expense-tracker/src/components/Expenses/Expenses.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+import "./Expenses.css";
+import ExpensesList from "./ExpensesList";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesChart from "./ExpensesChart";
+import Card from "../UI/Card";
+
+function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.data.filter((expense) => {
+    return expense.date.toString().slice(11, 15) === filteredYear;
+  });
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          onChangeFilter={filterChangeHandler}
+          selected={filteredYear}
+        />
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList data={filteredExpenses} />
+      </Card>
+    </div>
+  );
+}
+
+export default Expenses;
