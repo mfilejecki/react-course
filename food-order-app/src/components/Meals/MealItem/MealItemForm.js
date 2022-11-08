0aaa260dfbe089d71fe7713d8878461cb0123568
Marcel Filejecki <filej1025@gmail.com>
2022-11-08 21:33:47 +0100

    added more advanced logic to cart

--- food-order-app/src/components/Meals/MealItem/MealItemForm.js
+++ food-order-app/src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,29 @@
+import { useRef, useState } from "react";
+
 import styles from "./MealItemForm.module.css";
 
 import Input from "../../UI/Input";
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+
   const submitHandler = (event) => {
     event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNum = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNum < 1 ||
+      enteredAmountNum > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNum);
   };
 
   const inputSettings = {
@@ -18,8 +37,9 @@ const MealItemForm = (props) => {
 
   return (
     <form className={styles.form} onSubmit={submitHandler}>
-      <Input label="Amount" input={inputSettings} />
+      <Input label="Amount" input={inputSettings} ref={amountInputRef} />
       <button>+ Add</button>
+      {!amountIsValid && <p>Please render a valid amount (1-5).</p>}
     </form>
   );
 };
